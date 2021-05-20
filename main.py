7531ab58b930e34dac89858ad72b47b19752793d
Jason Yang <canyoncrestjy@gmail.com>
2021-05-20 14:39:54 -0700

    cross connections

---
+++ main.py
@@ -0,0 +1,13 @@
+import subprocess
+import pyautogui
+import time
+import pandas as pd
+from datetime import datetime
+
+def sign_in(meetingid, pwd):
+    #initiates the zoom app 
+    #subprocess.call(["/usr/bin/open", '/Users/canyo/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/zoom.us.app'])
+    print("hello")
+    #time.sleep(10) #gives adequate time for program to start next step
+
+sign_in(95200673544, 7377384)
