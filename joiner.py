e90e17678a70efef30ea669d725eecd326210211
Jason Yang <canyoncrestjy@gmail.com>
2021-05-20 14:31:32 -0700

    updated

--- joiner.py
+++ joiner.py
@@ -6,6 +6,6 @@ from datetime import datetime
 
 def sign_in(meetingid, pwd):
     #initiates the zoom app 
-    subprocess.call(["/usr/bin/open", '/Applications/zoom.us.app'])
+    subprocess.call(["/usr/bin/open", '/Users/canyo/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/zoom.us.app'])
 
-sign_in()
+    time.sleep(10) #gives adequate time for program to start next step
