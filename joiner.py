0975e2bb34fb53287913c90c8c23c74ed6c0329b
Jason Yang <canyoncrestjy@gmail.com>
2021-05-20 15:00:37 -0700

    deleted useless shit

--- joiner.py
+++ joiner.py
@@ -9,5 +9,12 @@ def sign_in(meetingid, pwd):
     subprocess.call("C:\\Users\\canyo\\AppData\\Roaming\\Zoom\\bin\\Zoom.exe")
     #print("hello")
     #time.sleep(10) #gives adequate time for program to start next step
+    while True:
+        joinbutton = pyautogui.locateOnScreen('joinbutton.png')
+        if joinbutton != None:
+            pyautogui.click(joinbutton)
+            print("Clicked the Join Button on Zoom")
+        else: 
+            print("Could not locate join button on Zoom")
 
 sign_in(95200673544, 7377384)
