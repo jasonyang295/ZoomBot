6fe7a9fc7fd39117dc8bb1491535959d488e75aa
Jason Yang <canyoncrestjy@gmail.com>
2021-05-20 15:02:28 -0700

    updated break condition and finished the loop

--- joiner.py
+++ joiner.py
@@ -4,6 +4,10 @@ import time
 import pandas as pd
 from datetime import datetime
 
+#temporary login creds: 
+#id: 95200673544
+#pwd: 7377384
+
 def sign_in(meetingid, pwd):
     #initiates the zoom app 
     subprocess.call("C:\\Users\\canyo\\AppData\\Roaming\\Zoom\\bin\\Zoom.exe")
@@ -14,6 +18,7 @@ def sign_in(meetingid, pwd):
         if joinbutton != None:
             pyautogui.click(joinbutton)
             print("Clicked the Join Button on Zoom")
+            break
         else: 
             print("Could not locate join button on Zoom")
 
