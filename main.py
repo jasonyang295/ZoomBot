94703a9e4c41a524f37b1261c8dcba2e52203b01
Jason Yang <canyoncrestjy@gmail.com>
2021-05-20 14:43:04 -0700

    program can open zoom now

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from datetime import datetime
 
 def sign_in(meetingid, pwd):
     #initiates the zoom app 
-    #subprocess.call(["/usr/bin/open", '/Users/canyo/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/zoom.us.app'])
-    print("hello")
+    subprocess.call("C:\\Users\\canyo\\AppData\\Roaming\\Zoom\\bin\\Zoom.exe")
+    #print("hello")
     #time.sleep(10) #gives adequate time for program to start next step
 
 sign_in(95200673544, 7377384)
